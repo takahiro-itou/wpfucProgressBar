5fd543f8b22ac1aad0924bb92e0ae5515c8e6fd0
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-07-04 22:28:28 +0900

    ユーザーコントロールにプロパティを追加
    (1) データバインディングの対象オブジェクト（ビューモデル）

--- ProgressControl.xaml.cs
+++ ProgressControl.xaml.cs
@@ -36,6 +36,19 @@ public  partial class  ProgressControl : UserControl
 //    外部に公開するプロパティ
 //
 
+    //----------------------------------------------------------------
+    /**   ViewModel プロパティ。
+    **
+    **/
+    public  IProgressViewModel?  ViewModel
+    {
+        get { return  this.m_viewModel; }
+        set {
+            this.m_viewModel = value;
+            this.DataContext = value;
+        }
+    }
+
 //========================================================================
 //
 //    外部に公開するイベント
