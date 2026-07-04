f2389c953ca04faa811027baa4a0900b644f02d9
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-07-04 11:16:42 +0900

    空のユーザーコントロールを新規作成

---
+++ ProgressControl.xaml.cs
@@ -0,0 +1,27 @@
+﻿//  -*-  coding: utf-8-with-signature-unix;        -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  WPF UserControl Library.  ---                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+using System.ComponentModel;
+using System.Windows.Controls;
+
+
+namespace  WpfControl.Utils  {
+
+public  partial class  ProgressControl : UserControl
+{
+
+
+}   //  End class ProgressControl
+
+}   //  End of namespace  WpfControl.Sample
